19ee7dcab1a06b783a76a60d10d2e944161e1e9f
Vu Tran <baysao@gmail.com>
2022-12-01 13:20:02 +0700

    update files

--- js/router.js
+++ js/router.js
@@ -1,48 +1,60 @@
-const route = (event) => {
-  event = event || window.event;
-  event.preventDefault();
-  window.history.pushState({}, "", event.target.href);
-  handleLocation();
-};
-// const githack_version = "71878f3882ba55d49099286b2c0094c8d96efd24";
 const githack_version = "main";
-const githack = "https://rawcdn.githack.com/baysao/baysaost/" + githack_version;
+const githack = "https:rawcdn.githack.com/baysao/baysaost/" + githack_version;
 
-const routes = {
-  404: "/pages/404.html",
-  "/": "/pages/index.html",
-  "#about": "/pages/about.html",
-  "#lorem": "/pages/lorem.html",
-  "#test": "/pages/test.html",
-  "#test1": "/pages/test1.dot",
-  "#test11": "/pages/test1.dot",
-};
-
+// handleLocation();
 const def = {
   header: "My Header",
 };
-const handleLocation = async () => {
-  const path = window.location.pathname;
-  const pathHash = window.location.hash;
-  console.log(pathHash);
-  let route = "";
-  if (pathHash) route = githack + routes[pathHash] || routes[404];
-  else route = githack + routes[path] || routes[404];
-  console.log(route);
-  let html = await fetch(route).then((data) => data.text());
-  if (/\.dot/.test(route)) {
+
+const route = Rlite(notFound, {
+  // Default route
+  "": function () {
+    return "Home";
+  },
+
+  about: function () {
+    return "About";
+  },
+  test1: async function () {
+    //    return "About";
+    let route = githack + "/pages/test1.dot";
+    const _html = await fetch(route).then((data) => data.text());
     const _json = await fetch(route.replace(/\.dot$/, ".json")).then((data) =>
       data.json()
     );
-    console.log(_json);
-    console.log("dot");
-    var pagefn = doT.template(html, undefined, def);
-    html = pagefn(_json);
-  }
+
+    var _tmpl = doT.template(_html, undefined, def);
+    return _tmpl(_json);
+  },
+
+  // // #sent?to=john -> r.params.to will equal 'john'
+  // sent: function ({ to }) {
+  //   return "Sent to " + to;
+  // },
+
+  // // #users/chris -> r.params.name will equal 'chris'
+  // "users/:name": function ({ name }) {
+  //   return "User " + name;
+  // },
+
+  // // #users/foo/bar/baz -> r.params.path will equal 'foo/bar/baz'
+  // "users/*path": function ({ path }) {
+  //   return "Path = " + path;
+  // },
+});
+
+function notFound() {
+  return "<h1>404 Not found :/</h1>";
+}
+
+// Hash-based routing
+async function processHash() {
+  const hash = location.hash || "#";
+
+  // Do something useful with the result of the route
+  let html = await route(hash.slice(1));
   document.getElementById("main-page").innerHTML = html;
-};
+}
 
-window.onpopstate = handleLocation;
-window.route = route;
-
-handleLocation();
+window.addEventListener("hashchange", processHash);
+processHash();
